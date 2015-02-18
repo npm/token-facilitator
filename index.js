204622c83bab4c632bd26c6a2d29fce96df4de27
rockbot <raquel@rckbt.me>
2015-02-17 17:16:06 -0800

    first attempt plus tests yay!

---
+++ index.js
@@ -0,0 +1,54 @@
+var assert = require('assert'),
+    crypto = require('crypto'),
+    _ = require('lodash');
+
+var Facilitator = module.exports = function (opts) {
+  _.extend(this, {}, opts);
+
+  assert(this.redis, 'we require a redis instance');
+
+  if (!this.logger) {
+    this.logger = {
+      error: console.error,
+      info: console.log
+    };
+  }
+
+  return this;
+};
+
+Facilitator.prototype.generate = function (data, options, cb) {
+  var _this = this;
+
+  if (typeof options === 'function') {
+    cb = options;
+    options = null;
+  }
+
+  var token = crypto.randomBytes(30).toString('base64')
+            .split('/').join('_')
+            .split('+').join('-'),
+      hash = sha(token),
+      key = (options && options.prefix || '') + hash;
+
+  data.token = token + '';
+
+  this.redis.set(key, JSON.stringify(data), function (err) {
+
+    if (err) {
+      _this.logger.error('Unable to set ' + key + ' to the cache');
+      _this.logger.error(err);
+      return cb(err);
+    }
+
+    if (options && options.timeout) {
+      _this.redis.expire(key, options.timeout);
+    }
+
+    return cb(null, key);
+  });
+};
+
+function sha (token) {
+  return crypto.createHash('sha1').update(token).digest('hex');
+}
---
+++ test/index.js
@@ -0,0 +1,145 @@
+var Code = require('code'),
+    Lab = require('lab'),
+    lab = exports.lab = Lab.script(),
+    describe = lab.experiment,
+    before = lab.before,
+    after = lab.after,
+    it = lab.test,
+    expect = Code.expect;
+
+var Facilitator = require('../.');
+
+var redis = require('redis'),
+    spawn = require('child_process').spawn,
+    client;
+
+var redisProcess;
+
+before(function (done) {
+  var redisConfig = '--port ' + 6379;
+  redisProcess = spawn('redis-server', [redisConfig]);
+
+  client = redis.createClient(6379, '127.0.0.1');
+  done();
+});
+
+after(function(done) {
+  redisProcess.kill('SIGKILL');
+  done();
+});
+
+describe('Instantiating a facilitator', function () {
+  it('errors out if there is no redis option', function (done) {
+    expect(function () {
+      var facilitator = new Facilitator();
+    }).to.throw('we require a redis instance');
+    done();
+  });
+
+  it('does not error out if redis is included', function (done) {
+    expect(function () {
+      var facilitator = new Facilitator({redis: client});
+    }).to.not.throw();
+    done();
+  });
+});
+
+describe('generating a token', function () {
+  it('sets some data to a randomly generated token', function (done) {
+    var data = 'boom';
+
+    var facilitator = new Facilitator({redis: client});
+    facilitator.generate(data, function (err, key) {
+      expect(err).to.not.exist();
+      expect(key).to.exist();
+      client.get(key, function (err, data) {
+        data = JSON.parse(data);
+        expect(data).to.equal('boom');
+        done();
+      });
+    });
+  });
+
+  it('includes the token if there data is an object', function (done) {
+    var data = {
+      a: 'one',
+      b: 'two'
+    };
+
+    var facilitator = new Facilitator({redis: client});
+    facilitator.generate(data, function (err, key) {
+      expect(err).to.not.exist();
+      expect(key).to.exist();
+      client.get(key, function (err, data) {
+        data = JSON.parse(data);
+        expect(data.token).to.exist();
+        done();
+      });
+    });
+  });
+
+  it('stringifies the data to be set', function (done) {
+    var data = {
+      a: 'one',
+      b: 'two'
+    };
+
+    var facilitator = new Facilitator({redis: client});
+    facilitator.generate(data, function (err, key) {
+      expect(err).to.not.exist();
+      expect(key).to.exist();
+      client.get(key, function (err, data) {
+        expect(data).to.be.a.string();
+        data = JSON.parse(data);
+        expect(data.a).to.equal('one');
+        expect(data.b).to.equal('two');
+        done();
+      });
+    });
+  });
+
+  it('adds a prefix to the key if desired', function (done) {
+    var data = 'boom';
+    var opts = {
+      prefix: 'ahoy:'
+    };
+
+    var facilitator = new Facilitator({redis: client});
+    facilitator.generate(data, opts, function (err, key) {
+      expect(err).to.not.exist();
+      expect(key).to.include('ahoy:');
+      client.get(key, function (err, data) {
+        data = JSON.parse(data);
+        expect(data).to.equal('boom');
+        done();
+      });
+    });
+  });
+
+  it('adds a timeout to the key if desired', function (done) {
+    var data = 'boom';
+    var opts = {
+      timeout: 1
+    };
+
+    var facilitator = new Facilitator({redis: client});
+    facilitator.generate(data, opts, function (err, key) {
+      expect(err).to.not.exist();
+      client.ttl(key, function (err, time) {
+        expect(time).to.equal(1);
+        client.get(key, function (err, data) {
+          data = JSON.parse(data);
+          expect(data).to.equal('boom');
+
+          setTimeout(function () {
+            client.get(key, function (err, data) {
+              data = JSON.parse(data);
+              expect(data).to.be.null;
+              done();
+            });
+          }, 1000);
+        });
+      });
+    });
+  });
+});
